5df2823dc82d555c4cbf780b9f5e72405a6235a1
Shabab Ayub <s4ayub@edu.uwaterloo.ca>
2020-03-26 00:38:54 -0400

    q4

---
+++ lab2q4_1.m
@@ -0,0 +1,9 @@
+% Sequential Discriminants (Q4)
+load('lab2_3.mat');
+
+% 1. Let a and b represent the data points in classes A and B. Let j = 1.
+j = 1;
+
+% 2. Randomly select one point from a and one point from b
+
+
