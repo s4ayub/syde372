2e6cc8db4e41488c8c45bd7311b6e777a6e51e4d
Shan Gao <s57gao@uwaterloo.ca>
2020-03-26 23:04:49 -0400

    plot discriminants, calculate errors, rename things to be shorter

--- lab2q4_1.m
+++ lab2q4_1.m
@@ -6,55 +6,55 @@ load('lab2_3.mat');
 A = a;
 B = b;
 
-discriminants_a = {};
-discriminants_b = {};
-all_num_wrong_a = {};
-all_num_wrong_b = {};
+disc_a = {};
+disc_b = {};
+num_wrong_a = {};
+num_wrong_b = {};
 
 % 1. Let a and b represent the data points in classes A and B. Let j = 1.
 j = 1;
-limit_num_classifiers = 6;
+limit_num_classifiers = 1;
 
-while (size(a,1) > 0 && size(b,1) > 0) && j < (limit_num_classifiers + 1)
-    misclassified_a = 1;
-    misclassified_b = 1;
-    while (sum(misclassified_a>0) > 0 && sum(misclassified_b>0) > 0)
+while (size(a,1) > 0 && size(b,1) > 0 && j <= limit_num_classifiers)
+    misclass_a = 1;
+    misclass_b = 1;
+    while (sum(misclass_a>0) > 0 && sum(misclass_b>0) > 0)
         % 2. Randomly select one point from a and one point from b
         a_point = get_random_point(a);
         b_point = get_random_point(b);
 
         % 3. Create a discriminant G using MED with the two points as prototypes
         % 4. Using all of the data in a and b, work out the confusion matrix entries
-        misclassified_a = get_misclassified_at_index(a, a_point, b_point, 1);
-        misclassified_b = get_misclassified_at_index(b, a_point, b_point, 0);
+        misclass_a = get_misclassified_at_index(a, a_point, b_point, 1);
+        misclass_b = get_misclassified_at_index(b, a_point, b_point, 0);
 
         %5. If num_wrong_a != 0 and num_wrong_b != 0 then no good, go back to step 2.
     end
 
-    num_wrong_a = sum(misclassified_a>0);
-    num_wrong_b = sum(misclassified_b>0);
+    curr_num_wrong_a = sum(misclass_a>0);
+    curr_num_wrong_b = sum(misclass_b>0);
     % 6. This discriminant is good; save it
-    discriminants_a = [discriminants_a, a_point];
-    discriminants_b = [discriminants_b, b_point];
-    all_num_wrong_a = [all_num_wrong_a, num_wrong_a];
-    all_num_wrong_b = [all_num_wrong_b, num_wrong_b];  
+    disc_a = [disc_a, a_point];
+    disc_b = [disc_b, b_point];
+    num_wrong_a = [num_wrong_a, curr_num_wrong_a];
+    num_wrong_b = [num_wrong_b, curr_num_wrong_b];  
 
     % 7. If num_wrong_a = 0 then remove those points from b that G classifies as B.
-    if (num_wrong_a == 0)
-        b = remove_points(b, misclassified_b);
+    if (curr_num_wrong_a == 0)
+        b = remove_points(b, misclass_b);
     end
 
     % 8. If num_wrong_b = 0 then remove those points from a that G classifies as A.
-    if (num_wrong_b == 0)
-        a = remove_points(a, misclassified_a);
+    if (curr_num_wrong_b == 0)
+        a = remove_points(a, misclass_a);
     end
     
     % 9. If a and b still contain points, go back to step 2.
     j = j + 1;
 end
 
-all_num_wrong_a = cell2mat(all_num_wrong_a);
-all_num_wrong_b = cell2mat(all_num_wrong_b);
+num_wrong_a = cell2mat(num_wrong_a);
+num_wrong_b = cell2mat(num_wrong_b);
 
 [xgrid, ygrid] = get_meshgrid(A, B);
 length = size(xgrid, 1) * size(xgrid,2);
@@ -65,7 +65,7 @@ for x = 1:size(xgrid, 2)
     for y = 1:size(ygrid, 1)
         xval = xgrid(1,x);
         yval = ygrid(y,1);
-        predicted(1,index) = classify_point([xval, yval], discriminants_a, discriminants_b, all_num_wrong_a, all_num_wrong_b);
+        predicted(1,index) = classify_point([xval, yval], disc_a, disc_b, num_wrong_a, num_wrong_b);
         index = index +1;
     end
 end
@@ -73,7 +73,7 @@ end
 x = 0.8;
 light_rb = [1 x x; x x 1];
 colors = [1 x x; x x 1; x 1 x];
-gscatter(xgrid(:), ygrid(:), predicted, colors);
+myscatter = gscatter(xgrid(:), ygrid(:), predicted, colors);
 hold on;
 a_plot = plot(A(:,1), A(:, 2), 'o', 'color', 'red', 'MarkerSize',3);
 hold on
@@ -81,9 +81,72 @@ b_plot = plot(B(:,1), B(:, 2), 'o', 'color', 'blue', 'MarkerSize',3);
 title("Sequential Discriminants Classifier",'FontSize',15);
 ylabel("x2");
 xlabel("x1");
-legend([a_plot, b_plot], "Class A", "Class B", "Location", 'northeast');
-axis tight
+% legend([a_plot, b_plot], "Class A", "Class B", "Location", 'northeast');
+plot_disc(disc_a, disc_b);
+limit_axes(A,B);
+error = calculate_error(A, B, disc_a, disc_b, num_wrong_a, num_wrong_b)
 
+function limit_axes(A, B)
+    maxXY = max(max(A),max(B));
+    minXY = min(min(A), min(B));
+    xlim([minXY(1) maxXY(1)]);
+    ylim([minXY(2) maxXY(2)]);
+end
+
+function error_percentage = calculate_error(A, B, disc_a, disc_b, num_wrong_a, num_wrong_b)
+A_size = size(A, 1);
+B_size = size(B, 1);
+
+A_class = zeros(1, A_size);
+B_class = zeros(1, B_size);
+
+for i=1:A_size
+   point = A(i,:)
+   A_class(1,i) = classify_point(point, disc_a, disc_b, num_wrong_a, num_wrong_b);
+end
+
+for i=1:B_size
+   point = B(i,:)
+   B_class(1,i) = classify_point(point, disc_a, disc_b, num_wrong_a, num_wrong_b);
+end
+
+errors = sum(A_class ~= 1) + sum(B_class ~= 2);
+error_percentage = errors/(A_size + B_size);
+end
+
+function plot_disc(disc_a, disc_b)
+    colors = {'k','g','y','c','m',[.5 .6 .7],[.8 .2 .6]}; % Cell array of colros.
+    for i=1:size(disc_a, 2)
+        A = disc_a{i}; %[x,y]
+        B = disc_b{i}; %[x,y]
+        Clen = 500; % distance off the line AB that C will lie
+        % Call AB the vector that points in the direction
+        % from A to B.
+        AB = B - A;
+        % Normalize AB to have unit length
+        AB = AB/norm(AB);
+        % compute the perpendicular vector to the line
+        % because AB had unit norm, so will ABperp
+        ABperp = AB*[0 -1;1 0];
+        % midpoint between A and B
+        ABmid = (A + B)/2;
+        % Compute new points C and D, each at a ditance
+        % Clen off the line. Note that since ABperp is
+        % a vector with unit eEuclidean norm, if I
+        % multiply it by Clen, then it has length Clen.
+        C = ABmid + Clen*ABperp;
+        D = ABmid - Clen*ABperp;
+        % plot them all
+        hold on
+        color = colors{i};
+        plot([A(1);B(1)],[A(2);B(2)],'color', color, 'marker', 's');
+        plot([C(1);D(1)],[C(2);D(2)], 'color', color)
+        % axis equal is important because it ensures the lines appear
+        % mutually perpendicular. If the axes had different units
+        % along the axes, then the lines would look skewed.
+        axis equal
+    end
+end
 
 function point = get_random_point(values)
     length = size(values,1);
@@ -141,10 +204,10 @@ end
  end
  
  function group = classify_point(point, disc_a, disc_b, num_wrong_a, num_wrong_b)
-    group = 3;
+    group = 0;
     % 1. Let j = 1
     for j=1:size(num_wrong_a, 2)
-        isClassA = med_classify(point, cell2mat(disc_a(j)), cell2mat(disc_b(j)));
+        isClassA = med_classify(point, disc_a{j}, disc_b{j});
         % 2. If Gj classifies x as class B and num_wrong_a,j = 0 then “Say Class B”
         if (isClassA == 0 && num_wrong_a(j) == 0 )
             group = 2;
@@ -157,7 +220,7 @@ end
         % 4. Otherwise j = j + 1 and go back to step 2.
     end
     
-%     if (group == 3)
+%     if (group == 0)
 %         disp("Something is wrong")
 %     end
 end
